b04dfff85e934547b638d0c5f4fa16a83a58db5b
Ofir Cohen <ofir.cohen@pipl.com>
2023-05-18 07:06:55 +0000

    add aiofiles

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="PythonIPQSDBReader",
-    version="1.0.5",
+    version="2.0.0",
     author="IPQualityScore",
     author_email="support@ipqualityscore.com",
     description="IPQualityScore IP Address Reputation & Proxy Detection DB Reader",
@@ -13,9 +13,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/IPQualityScore/PythonIPQSDBReader",
     packages=setuptools.find_packages(),
+    install_requires=["aiofiles==23.1.0"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ]
+    ],
 )
